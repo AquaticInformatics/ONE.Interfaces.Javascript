03af92d653dfeda2ac2ad218ee4d3e454aed1f46
Dan Gossner <dgossner@hach.com>
2022-03-23 11:59:14 -0500

    update path in generateJS file

--- generation/generateJS.sh
+++ generation/generateJS.sh
@@ -11,10 +11,10 @@ mkdir -p ./out/js
 # == pull dependencies ==
 npm install
 
-PROTOPATHS=`find ../claros.interfaces.protocolbuffers -type d -print | xargs -n 1 -I {} echo "-p {}" | xargs echo`
+PROTOPATHS=`find ../one.interfaces.protocolbuffers -type d -print | xargs -n 1 -I {} echo "-p {}" | xargs echo`
 
 # == compile with protobufjs ==
-for P in `find ../claros.interfaces.protocolbuffers/proto/one -name "*.proto"`
+for P in `find ../one.interfaces.protocolbuffers/proto/one -name "*.proto"`
 do
 	PROTO=`basename -s .proto ${P}`
 	echo "=== ${P} :: (${PROTO})"
