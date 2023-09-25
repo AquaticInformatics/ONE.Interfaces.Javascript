bec288fb9da7bbb5b1ebed76331ea132fc7c50be
Nicholas Kellerhals <nicholas.kellerhals@aquaticinformatics.com>
2023-09-25 09:55:15 -0600

    AQI-5737: Updated script and packages to point to new repo

--- generation/genAndPublish.sh
+++ generation/genAndPublish.sh
@@ -13,10 +13,7 @@ mkdir -p ../lib/src
 cp -p ./out/js/apiresponse.js ../lib/src/apiresponse.js
 cp -p ./out/ts/apiresponse.d.ts ../lib/src/apiresponse.d.ts
 
-# == configure @aqi scope to point at AQI ProGet registry ==
-npm config set @aqi:registry https://pkgs.dev.azure.com/aqi-devops/23daa24c-9d36-459f-8fc3-3a4ffa9de92a/_packaging/aqi-npm/npm/registry/
-
 # == pull lib dependencies and publish ==
 cd ../lib
 npm install
-npm publish
+npm publish --access public
